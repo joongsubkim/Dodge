44cb1099b26c7100eea795ad9e39ac3662099483
jongsub-kim <nomjr@daum.net>
2023-04-21 12:45:25 +0900

    20230421
    20230421

--- Dodge/Assets/bullet.cs
+++ Dodge/Assets/bullet.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+
+    public GameObject bulletPrefab; //생성할 탄알의 원본 프리팹
+    public float spawnerRateMin = 0.5f;
+    public float spawnerMax = 3f;
+
+    private Transform target; // 발사항 대상
+    private float spawnRate; //생성주기
+    private float timeAfterSpawn; //최근 생성 시점에서 지난 시간
+
+
     public float speed = 8f; //탄알 이동 속력
     private Rigidbody bulletRigidbody; //이동에 사용할 리지드바디 컴포넌트
 
-       
+
     void Start()
     {
         //게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 bulletRigidbody에 할당
@@ -16,9 +26,23 @@ public class bullet : MonoBehaviour
         Destroy(gameObject, 3f);
     }
 
- 
-    void Update()
+    //트리거충돌시 자동으로 실행되는 메서드
+    private void OnTriggerEnter(Collider other)
     {
-        
+        //충돌한 상대방 게임 오브젝트가 player태그를 가진 경우
+        if (other.tag == "Player")
+        {
+            playerController PlayerController = other.GetComponent<playerController>();
+            //상대방 게임 오브젝트에서 playerController 컴퍼넌트를 가져오는 데 성공했다면
+            if (PlayerController != null)
+            {
+                //상대방 playerController 컴포넌트의 Die() 메서드 실행
+                PlayerController.Die();
+            }
+
+        }
     }
 }
+    
+
+    
--- Dodge/Assets/playerController.cs
+++ Dodge/Assets/playerController.cs
@@ -29,37 +29,12 @@ public class playerController : MonoBehaviour
         playerRigidbody.velocity = newVelocity;
 
     }
+
+    public void Die()
+    {
+        // 자신의 게임 오브젝트를 비활성화
+        // 컴포넌트 활성화/ 비활성화 : enabled
+        gameObject.SetActive(false);
+
+    }
 }
-           
-        
-        
-        //if (Input.GetKey(KeyCode.UpArrow) == true)
-        //{
-        //    //왼쪽으로 방향키 입력이 감지된 경우 z 방향 힘 주기
-        //    playerRigidbody.AddForce(0f, 0f, speed);
-        //}
-        //if (Input.GetKey(KeyCode.DownArrow) == true)
-        //{
-        //    //아래쪽 방향키 입력이 감지된 경우-z방향 힘 주기
-        //    playerRigidbody.AddForce(0f, 0f,-speed);
-
-        //}
-        //if (Input.GetKey(KeyCode.RightArrow) == true)
-        //{
-        //    //오른쪽 방향키 입력이 감지된 경우 x방향 힘 주기
-        //    playerRigidbody.AddForce(speed, 0f, 0f);
-        //}
-        //if (Input.GetKey(KeyCode.LeftArrow) == true)
-        //{
-        //    //왼쪽 방향키 입력이 감지된 경우 -x방향 힘 주기
-        //    playerRigidbody.AddForce(-speed, 0f, 0f);
-        //}
-    //}
-   // public void Die()
-    //{
-        //자신의 게임 오브젝트를 비활성화
-        // 컴포넌트 활성화/비활성화 : enabled
-     //   gameObject.SetActive(false);
-   // }
-  //}
-
